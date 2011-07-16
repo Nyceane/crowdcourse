5150a7f445378b5215aa88702895a6a15dffed8a
Peter Ma <peterma@Peter-Mas-MacBook-Pro-2.local>
2011-07-15 23:52:45 -0700

    first commit

---
+++ Resources/xibs/CartController.h
@@ -0,0 +1,13 @@
+//
+//  CartController.h
+//  QuickSignIn
+//
+//  Created by Peter Ma on 7/15/11.
+//  Copyright 2011 Catch.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CartController : UIViewController
+
+@end
